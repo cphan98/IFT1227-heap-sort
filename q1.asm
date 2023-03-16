78e75b89462c6e0e421b279c29ae8fbe2e802cd8
Cynthia Phan <cynthiaphan.code@gmail.com>
2023-03-16 15:57:10 -0400

    add code to get user's input

--- q1.asm
+++ q1.asm
@@ -1,2 +1,20 @@
 # 20220019 Hoang-Thi-Thi Cynthia Phan
 # 20254813 Razafindrakoto Nathan Riantsoa
+
+# initialize array of n elements
+
+.data
+    prompt: .asciiz "Enter the desired length of the array: "
+
+.text
+    # prompt user to enter length n
+    li  $v0, 4          # $v0 = 4
+    la  $a0, prompt     # $a0 = prompt
+    syscall
+
+    # get user's desired length n
+    li		$v0, 5	    # $v0 = 5
+    syscall
+
+    # store user's input into $s0
+    move 	$t0, $v0	# $t0 = $v0
