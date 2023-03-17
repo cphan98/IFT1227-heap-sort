215a7a799a2254f2c3d8bcbae6a056954bfbbcad
Cynthia Phan <cynthiaphan.code@gmail.com>
2023-03-17 19:57:59 -0400

    code getLeftChildIndex

--- q1.asm
+++ q1.asm
@@ -6,65 +6,79 @@
 prompt: .asciiz "Enter the desired array length: "
 address: .asciiz "Array base address: "
 length: .asciiz "\nArray length: "
+leftChild: .asciiz "\nLeft child index: "
 
-.data 0x10040000            # array starts at 0x10040000
+.data 0x10040000                # array starts at 0x10040000
 array:
 
 # functions and methods
 .text
 main:
-    li      $v0, 4          # syscall 4: print string
+    li      $v0, 4              # syscall 4: print string
     la      $a0, prompt     
-    syscall                 # print prompt
-    li	    $v0, 5	        # syscall 5: read int
-    syscall                 # $v0 = user's input
-    blt		$v0, $0, main	# if $v0 < 0, then go to main
-    jal		init			# jump to init and save position to $ra
-    addi    $a0, $0, 0      # argument 0 = i
-    addi    $a1, $0, 1      # argument 1 = j
-    jal     swap
+    syscall                     # print prompt
+    li	    $v0, 5	            # syscall 5: read int
+    syscall                     # $v0 = user's input
+    blt		$v0, $0, main	    # if $v0 < 0, then go to main
+    jal		init			    # jump to init and save position to $ra
+    addi    $a0, $0, 0          # argument 0 = i = 0
+    addi    $a1, $0, 1          # argument 1 = j = 1
+    jal     swap                # jump to swap and save position to $ra
+    addi    $a0, $0, 0          # argument 0 = index = 0
+    jal		getLeftChildIndex   # jump to getLeftChildIndex and save position to $ra
+    
     # TODO call other methods/functions
-    li      $v0, 10         # syscall 10: exit
-    syscall                 # terminate execution
+    li      $v0, 10             # syscall 10: exit
+    syscall                     # terminate execution
 
 init:
-    add     $t0, $0, $v0    # $t0 = user's input = counter for loop
-    add     $s0, $0, $t0    # $s0 = array length
-    la      $t1, array      # $t1 = 0x10040000
-loop:                       # loop to add values into array
-    li      $v0, 5          # syscall 5: read int 
-    syscall                 # $v0 = user's input
-    sw		$v0, 0($t1)		# array[i] = user's input
-    addi    $t1, $t1, 4     # go to next address
-    addi    $t0, $t0, -1    # counter = counter - 1
-    bgt     $t0, $0, loop	# if $t0 > 0 then go to loop
-    li      $v0, 4          # syscall 4: print string
+    add     $t0, $0, $v0        # $t0 = user's input = counter for loop
+    add     $s0, $0, $t0        # $s0 = array length
+    la      $t1, array          # $t1 = 0x10040000
+loop:                           # loop to add values into array
+    li      $v0, 5              # syscall 5: read int 
+    syscall                     # $v0 = user's input
+    sw		$v0, 0($t1)		    # array[i] = user's input
+    addi    $t1, $t1, 4         # go to next address
+    addi    $t0, $t0, -1        # counter = counter - 1
+    bgt     $t0, $0, loop	    # if $t0 > 0 then go to loop
+    li      $v0, 4              # syscall 4: print string
     la      $a0, address     
-    syscall                 # print address
-    li      $v0, 34         # syscall 34: print int in hexa
+    syscall                     # print address
+    li      $v0, 34             # syscall 34: print int in hexa
     la      $a0, array
-    syscall                 # print array address
-    li      $v0, 4          # syscall 4: print string
+    syscall                     # print array address
+    li      $v0, 4              # syscall 4: print string
     la      $a0, length     
-    syscall                 # print length
-    li      $v0, 1          # syscall 1: print int
+    syscall                     # print length
+    li      $v0, 1              # syscall 1: print int
     add     $a0, $0, $s0     
-    syscall                 # print array length
-    jr		$ra				# jump to $ra
+    syscall                     # print array length
+    jr		$ra				    # jump to $ra
 
 swap: 
     la		$t0, array
-    mul     $t1, $a0, 4     # position i in array
-    add     $t2, $t0, $t1   # array[i] address
-    lw		$t3, 0($t2)		# array[i]
-    mul     $t1, $a1, 4     # position j in array
-    add     $t4, $t0, $t1   # array[j] address
-    lw      $t5, 0($t4)     # array[j]
-    sw      $t5, 0($t2)     # store array[j] into array[i] address
-    sw      $t3, 0($t4)     # store array[i] into array[j] address
-    jr		$ra				# jump to $ra
+    mul     $t1, $a0, 4         # position i in array
+    add     $t2, $t0, $t1       # array[i] address
+    lw		$t3, 0($t2)		    # array[i]
+    mul     $t1, $a1, 4         # position j in array
+    add     $t4, $t0, $t1       # array[j] address
+    lw      $t5, 0($t4)         # array[j]
+    sw      $t5, 0($t2)         # store array[j] into array[i] address
+    sw      $t3, 0($t4)         # store array[i] into array[j] address
+    jr		$ra				    # jump to $ra
     
-# getLeftChildIndex:
-#     # TODO
+getLeftChildIndex:
+    # argument $a0 = parent index
+    mul     $t0, $a0, 2         # 2 * index
+    addi    $t0, $t0, 1         # 2 * index + 1
+    li      $v0, 4              # syscall 4: print string
+    la      $a0, leftChild     
+    syscall                     # print leftChihld
+    li      $v0, 1              # syscall 1: print int
+    add     $a0, $0, $t0     
+    syscall                     # print left child index
+    jr		$ra					# jump to $ra
+    
 # getRightChildIndex:
     # TODO
