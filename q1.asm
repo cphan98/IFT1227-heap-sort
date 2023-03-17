45685754a288bdf9a6c8389bbb1632f897aeb8b6
Cynthia Phan <cynthiaphan.code@gmail.com>
2023-03-17 16:43:25 -0400

    change registers $s0, $s1 to $t0, $t1 create variable array at 0x10040000

--- q1.asm
+++ q1.asm
@@ -5,6 +5,9 @@
 .data
 prompt: .asciiz "Enter the desired array length: "
 
+.data 0x10040000            # array starts at 0x10040000
+array:
+
 # functions and methods
 .text
 main:
@@ -13,18 +16,18 @@ main:
     syscall                 # print prompt
     li	    $v0, 5	        # syscall 5: read int
     syscall                 # $v0 = user's input
-    add     $s0, $0, $v0    # $s0 = $v0 = counter for loop
+    add     $t0, $0, $v0    # $t0 = $v0 = counter for loop
     # TODO call other methods/functions
 
 init:
-    la      $s1, 0x10040000 # $s1 = 0x10040000
+    la      $t1, array      # $t1 = 0x10040000
 loop:                       # loop to add values into array
     li      $v0, 5          # syscall 5: read int 
     syscall                 # $v0 = user's input
-    sw		$v0, 0($s1)		# array[i] = user's input
-    addi    $s1, $s1, 4     # go to next address
-    addi    $s0, $s0, -1    # counter = counter - 1
-    bgt     $s0, $0, loop	# if $s0 > 0 then go to loop
+    sw		$v0, 0($t1)		# array[i] = user's input
+    addi    $t1, $t1, 4     # go to next address
+    addi    $t0, $t0, -1    # counter = counter - 1
+    bgt     $t0, $0, loop	# if $s0 > 0 then go to loop
     # TODO return array length and address
 
 # swap:
