e885de81576a7e75951a1d7c69f262d5f9e0344e
Cynthia Phan <cynthiaphan.code@gmail.com>
2023-03-27 19:24:15 -0400

    update fixHeap function

--- heapsortTab.asm
+++ heapsortTab.asm
@@ -73,48 +73,21 @@ getRightChildIndex:                 # 1 argument: $a0 = index
     addi    $t9, $t9, 2         # $t9 = 2 * index + 2
     jr      $ra                 # jump to $ra
 
-fixHeap:                            # 2 arguments: $a0 = rootIndex, $a1 = lastIndex
-    # TODO
-    # 1. remove root
-    mul     $t0, $a0, 4         # $t0 = position rootIndex in array
-    add     $t1, $s1, $t0       # $t1 = array[rootIndex] address
-    lw      $s2, 0($t1)         # $s2 = array[rootIndex] = rootValue
-    # 2. promote children while they are larger than the root
-    add     $s3, $0, $a0        # $s3 = index
-    addi    $s4, $0, 1          # $s4 = more = 1
+fixHeap:							# 1 arg: $a0 = index
+	addi	$sp, $sp, -4			# make space in stack
+	sw		$ra, 0($sp)				# add $ra value to stack
+	# remove root
+	mul		$t0, $a0, 4				# $t0 = rootIndex position in array
+	add		$t1, $s1, $t0			# $t1 = array[rootIndex] address
+	lw		$s2, 0($t1)				# $s2 = array[rootIndex] = rootValue
+	# promote children while they are larger than the root
+	add		$s3, $0, $a0			# $s3 = index
+	addi	$s4, $0, 1				# $s4 = more = true = 1
 while:
-    bne     $s4, 1, done        # if $s4 != 1 then go to done
-    add     $a0, $0, $s3        # argument 0 : $a0 = index
-    la      $t7, ($ra)          # save previous $ra before jal
-    jal     getLeftChildIndex   # jump to getLeftChildIndex and save position to $ra
-    bgt     $t8, $a1, else      # if $t8 > $a1 then go to else
-    jal     getRightChildIndex  # jump to getRightChildIndex and save position to $ra
-    bgt     $t9, $a1, if        # if $t9 > $a1 then go to if
-    mul     $t0, $t9, 4         # $t0 = position rightChildIndex in array
-    add     $t1, $s1, $t0       # $t1 = array[rightChildIndex] address
-    lw      $t2, 0($t1)         # $t2 = array[rightChildIndex]
-    mul     $t0, $t8, 4         # $t0 = position childIndex
-    add     $t1, $s1, $t0       # $t1 = array[childIndex] address
-    lw      $t3, 0($t1)         # $t3 = array[childIndex]
-    ble     $t2, $t3, if        # if $t2 <= $t3 then go to if
-    add     $t8, $0, $t9        # childIndex = rightChildIndex
-if:
-    ble     $t3, $s2, else      # if $t3 <= $s2 then go to else
-    mul     $t0, $s3, 4         # $t0 = position index in array
-    add     $t1, $s1, $t0       # $t1 = array[index] address
-    sw      $t3, 0($t1)         # array[index] = array[childIndex]
-    add     $s3, $0, $t8        # index = childIndex
-    j       done                # jump to done
-else:
-    addi    $s4, $0, -1         # more = 0 = false
-    j       while               # jump to while
-    # 3. store root value in vacant slot
-done:
-    mul     $t0, $s3, 4         # $t0 = position index in array
-    add     $t1, $s1, $t0       # $t1 = array[index] address
-    sw      $s2, 0($t1)         # array[index] = rootValue
-    la      $ra, ($t7)
-    jr      $ra                 # jump to $ra
+	bne		$s4, 1, done			# if $s4 != 1 then go to done
+	add		$a0, $0, $s3			# arg 0 = $a0 = index
+	jal		getLeftChildIndex		# jump to getLeftChildIndex and save position to $ra
+    #TODO
 
 heapSort:						    # 2 arguments: $a0 = array, $a1 = array length
 	addi	$sp, $sp, -4		    # make space in stack
