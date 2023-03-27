e68f33ecd7107d9d2287db11d8ef87c7379158e8
Cynthia Phan <cynthiaphan.code@gmail.com>
2023-03-27 18:44:53 -0400

    reformat main code

--- heapsortTab.asm
+++ heapsortTab.asm
@@ -7,23 +7,23 @@ prompt: .asciiz "Enter the desired array length: "
 address: .asciiz "Array base address: "
 length: .asciiz "\nArray length: "
 
-.data 0x10040000			# array starts at 0x10040000
+.data 0x10040000			    # array starts at 0x10040000
 array:
 
 # functions and methods
 .text
 main:
-	li	$v0, 4			        # syscall 4: print string
-	la	$a0, prompt
+	li	    $v0, 4			    # syscall 4: print string
+	la	    $a0, prompt
 	syscall 			        # print "Enter the desired array length: "
-	li	$v0, 5			        # syscall 5: read int
+	li	    $v0, 5			    # syscall 5: read int
 	syscall				        # $v0 = user's input = array length
-	blt	$v0, $0, main	        # if $v0 < 0 (array length is neg), then go to main
+	blt	    $v0, $0, main	    # if $v0 < 0 (array length is neg), then go to main
 	jal	init			        # else, jump to init and save position to $ra
-	la	$a0, array		        # arg 0 = $a0 = array address base
-	add	$a1, $0, $v0	        # arg 1 = $a1 = array length		
+	la	    $a0, array		    # arg 0 = $a0 = array address base
+	add	    $a1, $0, $v0	    # arg 1 = $a1 = array length		
 	jal	heapsort		        # jump to init and save position to $ra
-	li	$v0, 10			        # syscall 10: exit
+	li	    $v0, 10			    # syscall 10: exit
 	syscall 			        # terminate execution
 
 init:
