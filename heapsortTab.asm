0d08e6f989a2010240638714b24ea9ce210448cc
Cynthia Phan <cynthiaphan.code@gmail.com>
2023-03-28 18:01:30 -0400

    code init

--- heapsortTab.asm
+++ heapsortTab.asm
@@ -11,6 +11,8 @@ length: .asciiz "\nArray length: "
 array:
 
 # functions and methods
+# $s0 = array length
+# $s1 = 0x10040000
 .text
 main:
     li      $v0,    4                               # syscall 4: print string
@@ -18,7 +20,33 @@ main:
     syscall                                         # print "Enter the desired array length: "
     li      $v0,    5                               # syscall 5: read int
     syscall                                         # $v0 = user's input = array lenght
-    blt     $v0,    $0,     main                    # if $v0 < 0 (array length is neg), then go to main
+    blt     $v0,    $0,         main                # if $v0 < 0 (array length is neg), then go to main
     jal     init                                    # else, jump to init and save position to $ra
 
 # TODO
+
+init:
+    add     $t0,    $0,         $v0                 # $t0 = array length = counter for loop
+    add     $s0,    $0,         $t0                 # $s0 = array length
+    la      $s1,    array                           # $s1 = 0x10040000
+initLoop:
+    li      $v0,    5                               # syscall 5: read int
+    syscall                                         # $v0 = user's input
+    sw      $v0,    0($s1)                          # store $v0 = user's input into $s1 address = array[i] address
+    addi    $s1,    $s1,        4                   # array[i] address + 4 = next array[i] address
+    addi    $t0,    $t0,        -1                  # $t0 = counter - 1
+    bgt     $t0,    $0,         initLoop            # if $t0 > 0 (counter is pos), then go to initLoop
+    la      $s1,    array                           # reset $s1 = 0x10040000
+    li      $v0,    4                               # syscall 4: print string
+    la      $a0,    address
+    syscall                                         # print "Array base address: "
+    li      $v0,    34                              # syscall 34: print int in hexadecimal
+    la      $a0,    array
+    syscall                                         # print 0x10040000
+    li      $v0,    4                               # syscall 4: print string
+    la      $a0,    length
+    syscall                                         # print "\nArray length: "
+    li      $v0,    1                               # syscall 1: print int
+    add     $a0,    $0,         $s0
+    syscall                                         # print $s0 = array length
+    jr      $ra                                     # jump to $ra
