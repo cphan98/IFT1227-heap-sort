f91554420af7b522822e8e28768f1c6ae38b2ace
Cynthia Phan <cynthiaphan.code@gmail.com>
2023-03-28 20:24:36 -0400

    finish fixHeap

--- heapsortTab.asm
+++ heapsortTab.asm
@@ -19,27 +19,27 @@ main:
     la      $a0,                prompt
     syscall                                             # print "Enter the desired array length: "
     li      $v0,                5                       # syscall 5: read int
-    syscall                                             # $v0 = user's input = array lenght
+    syscall                                             # $v0: user's input = array length
     blt     $v0,                $0,         main        # if $v0 < 0 (array length is neg), then go to main
     jal     init                                        # else, jump to init and save position to $ra
-    la      $a0,                array                   # arg 0 = $a0 = 0x10040000
-    add     $a1,                $0,         $s0         # arg 1 = $a1 = array length
+    la      $a0,                array                   # arg 0: $a0 = 0x10040000
+    add     $a1,                $0,         $s0         # arg 1: $a1 = array length
     jal     heapSort                                    # jump to heapSort and save position to $ra
 
 # TODO
 
 init:
-    add     $t0,                $0,         $v0         # $t0 = array length = counter for loop
-    add     $s0,                $0,         $t0         # $s0 = array length
-    la      $s1,                array                   # $s1 = 0x10040000
+    add     $t0,                $0,         $v0         # $t0: array length = counter for loop
+    add     $s0,                $0,         $t0         # $s0: array length
+    la      $s1,                array                   # $s1: 0x10040000
 initLoop:
     li      $v0,                5                       # syscall 5: read int
-    syscall                                             # $v0 = user's input
-    sw      $v0,                0($s1)                  # store $v0 = user's input into $s1 address = array[i] address
-    addi    $s1,                $s1,        4           # array[i] address + 4 = next array[i] address
-    addi    $t0,                $t0,        -1          # $t0 = counter - 1
+    syscall                                             # $v0: user's input = element in array
+    sw      $v0,                0($s1)                  # store $v0 (element) into $s1 address (array[i] address)
+    addi    $s1,                $s1,        4           # $s1: array[i] address + 4 = next array[i] address
+    addi    $t0,                $t0,        -1          # $t0: counter - 1
     bgt     $t0,                $0,         initLoop    # if $t0 > 0 (counter is pos), then go to initLoop
-    la      $s1,                array                   # reset $s1 = 0x10040000
+    la      $s1,                array                   # reset $s1 to 0x10040000
     li      $v0,                4                       # syscall 4: print string
     la      $a0,                address
     syscall                                             # print "Array base address: "
@@ -51,19 +51,19 @@ initLoop:
     syscall                                             # print "\nArray length: "
     li      $v0,                1                       # syscall 1: print int
     add     $a0,                $0,         $s0
-    syscall                                             # print $s0 = array length
+    syscall                                             # print $s0 (array length)
     jr      $ra                                         # jump to $ra
 
 # 1 arg: $a0 = index
 getLeftChildIndex:
-    mul     $s4,                $a0,        2           # $s4 = 2 * index
-    addi    $s4,                $s4,        1           # $s4 = 2 * index + 1
+    mul     $s4,                $a0,        2           # $s4: 2 * index
+    addi    $s4,                $s4,        1           # $s4: 2 * index + 1
     jr      $ra                                         # jump to $ra
 
 # 1 arg: $a0 = index
 getRightChildIndex:
-    mul     $s5,                $a0,        2           # $s5 = 2 * index
-    addi    $s5,                $s5,        2           # $s5 = 2 * index + 2
+    mul     $s5,                $a0,        2           # $s5: 2 * index
+    addi    $s5,                $s5,        2           # $s5: 2 * index + 2
     jr      $ra                                         # jump to $ra
 
 # 2 args: $a0 = rootIndex, $a1 = lastIndex
@@ -71,21 +71,51 @@ fixHeap:
     addi    $sp,                $sp,        -4          # make spce in stack
     sw      $ra,                0($sp)                  # add $ra value to stack
                                                         # remove root
-    mul     $s2,                $a0,        4           # $s2 = rootIndex position in array
-    add     $s2,                $s2,        $s1         # $s2 = array[rootIndex] address
-    lw      $s2,                0($s2)                  # $s2 = array[rootIndex] value = rootValue
+    mul     $s2,                $a0,        4           # $s2: rootIndex position in array
+    add     $s2,                $s2,        $s1         # $s2: array[rootIndex] address
+    lw      $s2,                0($s2)                  # $s2: rootValue = array[rootIndex] value
                                                         # promote children while they are larger than the root
-    add     $s3,                $0,         $a0         # $s3 = rootIndex = index
-    addi    $t2,                $0,         1           # $t2 = 1 = true = more
+    add     $s3,                $0,         $a0         # $s3: index = rootIndex
+    addi    $t2,                $0,         1           # $t2: more = true = 1
 while:
     bne     $t2,                1,          done        # if $t2 != 1 (more = false), then go to done
-    add     $a0,                $0,         $3          # arg 0 = $a0 = index
+    add     $a0,                $0,         $3          # arg 0: $a0 = index
     jal     getLeftChildIndex                           # jump to getLeftChildIndex and save position to $ra
     add     $s4,                $a1,        else        # if $s4 > $a1 (childIndex > lastIndex), then go to else
                                                         # use right child instead if it is larger
     jal     getRightChildIndex                          # jump to getRightChildIndex and save position to $ra
-
-# TODO
+    bgt     $s5,                $a1,        if          # if $s5 > $a1 (rightChildIndex > lastIndex), then go to if
+    mul     $t3,                $s5,        4           # $t3: rightChild position in array
+    add     $t3,                $t3,        $s1         # $t3: array[rightChildIndex] address
+    lw      $t3,                0($t3)                  # $t3: array[rightChildIndex] value
+    mul     $t4,                $s4,        4           # $t4: childIndex position in array
+    add     $t4,                $t4,        $s1         # $t4: array[childIndex] address
+    lw      $t4,                0($t4)                  # $t4: array[childIndex] value
+    ble     $t3,                $t4,        if          # if $t3 <= $t4 (array[rightChildIndex <= array[childIndex]), then go to if
+    add     $s4,                $0,         $s5         # $s4: childIndex = rightChildIndex
+if:
+    mul     $t4,                $s4,        4           # $t4: childIndex position in array
+    add     $t4,                $t4,        $s1         # $t4: array[childIndex] address
+    lw      $t4,                0($t4)                  # $t4: array[childIndex] value
+    ble     $t4,                $s2,        else        # if $t4 <= $s2 (array[childIndex] <= rootValue), then go to else
+                                                        # promote child
+    mul     $t5,                $s3,        4           # $t5: index position in array
+    add     $t5,                $t5,        $s1         # $t5: array[index] address
+    sw      $t4,                0($t5)                  # store $t4 (array[childIndex] value) into $t5 address (array[index] address)
+    add     $s3,                $0,         $s4         # $s3: index = childIndex
+    j       while                                       # jump to while
+else:
+                                                        # no children or root value is larger than both children
+    addi    $t2,                $0,         -1          # $t2: more = false = 0
+    j       while                                       # jump to while
+done:
+                                                        # store root value in vacant slot
+    mul     $t5,                $s3,        4           # $t5: index position in array
+    add     $t5,                $t5,        $s1         # $t5: array[index] address
+    sw      $s2,                0($t5)                  # store $s2 (rootValue) into $t5 address (array[index] address)
+    lw      $ra,                0($sp)                  # restore $ra
+    addi    $sp,                $sp,        4           # restore $sp
+    jr      $ra                                         # jump to $ra
 
 # 2 args: $a0 = array base address, $a1 = array length
 heapSort:
